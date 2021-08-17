c9e1720d54a86b5d92f323b47d5f225c5f5453b7
Chris Chamberlain <chris@chamberlain.id.au>
2021-08-17 23:30:07 +1000

    added more test components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,6 +31,12 @@
         <template v-slot:empty>No results found</template>
       </o-autocomplete>
     </o-field>
+
+    <o-table hoverable focusable :data="tableData" :columns="columns"></o-table>
+
+    <o-field label="Name">
+      <o-input></o-input>
+    </o-field>
   </div>
 </template>
 
@@ -55,7 +61,70 @@ export default defineComponent({
       })
     });
 
-    return { data, name, filteredDataArray }
+    const tableData = ref([
+          {
+            id: 1,
+            first_name: 'Jesse',
+            last_name: 'Simmons',
+            date: '2016-10-15 13:43:27',
+            gender: 'Male'
+          },
+          {
+            id: 2,
+            first_name: 'John',
+            last_name: 'Jacobs',
+            date: '2016-12-15 06:00:53',
+            gender: 'Male'
+          },
+          {
+            id: 3,
+            first_name: 'Tina',
+            last_name: 'Gilbert',
+            date: '2016-04-26 06:26:28',
+            gender: 'Female'
+          },
+          {
+            id: 4,
+            first_name: 'Clarence',
+            last_name: 'Flores',
+            date: '2016-04-10 10:28:46',
+            gender: 'Male'
+          },
+          {
+            id: 5,
+            first_name: 'Anne',
+            last_name: 'Lee',
+            date: '2016-12-06 14:38:38',
+            gender: 'Female'
+          }
+        ]);
+        const columns = ref([
+          {
+            field: 'id',
+            label: 'ID',
+            width: '40',
+            numeric: true
+          },
+          {
+            field: 'first_name',
+            label: 'First Name'
+          },
+          {
+            field: 'last_name',
+            label: 'Last Name'
+          },
+          {
+            field: 'date',
+            label: 'Date',
+            position: 'centered'
+          },
+          {
+            field: 'gender',
+            label: 'Gender'
+          }
+        ]);
+
+    return { name, filteredDataArray, tableData, columns }
   }
 });
 </script>
