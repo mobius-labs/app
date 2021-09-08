c740598f56f260fb658bf318e0c0481867a87987
Chris Chamberlain <chris@chamberlain.id.au>
2021-09-08 13:55:32 +1000

    Revert "Add a GitHub action to lint frontend code [MOBIUS-22] (#2)"
    This reverts commit 5c5c414919bc05870d7975e2b15f07d9d7548851.

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,228 +1,148 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-        <p>
-            For a guide and recipes on how to configure / customize this
-            project,<br />
-            check out the
-            <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-                >vue-cli documentation</a
-            >.
-        </p>
-        <h3>Installed CLI Plugins</h3>
-        <ul>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-                    target="_blank"
-                    rel="noopener"
-                    >typescript</a
-                >
-            </li>
-        </ul>
-        <h3>Essential Links</h3>
-        <ul>
-            <li>
-                <a href="https://vuejs.org" target="_blank" rel="noopener"
-                    >Core Docs</a
-                >
-            </li>
-            <li>
-                <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-                    >Forum</a
-                >
-            </li>
-            <li>
-                <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-                    >Community Chat</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://twitter.com/vuejs"
-                    target="_blank"
-                    rel="noopener"
-                    >Twitter</a
-                >
-            </li>
-            <li>
-                <a href="https://news.vuejs.org" target="_blank" rel="noopener"
-                    >News</a
-                >
-            </li>
-        </ul>
-        <h3>Ecosystem</h3>
-        <ul>
-            <li>
-                <a
-                    href="https://router.vuejs.org"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-router</a
-                >
-            </li>
-            <li>
-                <a href="https://vuex.vuejs.org" target="_blank" rel="noopener"
-                    >vuex</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-devtools#vue-devtools"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-devtools</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://vue-loader.vuejs.org"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-loader</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/awesome-vue"
-                    target="_blank"
-                    rel="noopener"
-                    >awesome-vue</a
-                >
-            </li>
-        </ul>
-        <o-field label="Find a JS framework">
-            <o-autocomplete
-                v-model="name"
-                rounded
-                expanded
-                open-on-focus
-                placeholder="e.g.: jQuery"
-                :data="filteredDataArray"
-                icon="search"
-                clearable
-                @select="(option) => (selected = option)"
-            >
-                <template #empty>No results found</template>
-            </o-autocomplete>
-        </o-field>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+    <o-field label="Find a JS framework">
+      <o-autocomplete rounded expanded open-on-focus v-model="name" placeholder="e.g.: jQuery" :data="filteredDataArray" icon="search" clearable @select="option => selected = option">
+        <template v-slot:empty>No results found</template>
+      </o-autocomplete>
+    </o-field>
 
-        <o-table
-            hoverable
-            focusable
-            :data="tableData"
-            :columns="columns"
-        ></o-table>
+    <o-table hoverable focusable :data="tableData" :columns="columns"></o-table>
 
-        <o-field label="Name">
-            <o-input></o-input>
-        </o-field>
-    </div>
+    <o-field label="Name">
+      <o-input></o-input>
+    </o-field>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from "vue";
+import { defineComponent, ref, computed } from 'vue';
 
 export default defineComponent({
-    props: {
-        msg: String,
-    },
-    setup() {
-        const data = ref(["awesome", "stuff", "going", "on"]);
-        const name = ref("");
-        const filteredDataArray = computed(() => {
-            return data.value.filter((option) => {
-                return (
-                    option
-                        .toString()
-                        .toLowerCase()
-                        .indexOf(name.value.toLowerCase()) >= 0
-                );
-            });
-        });
+  props: {
+    msg: String
+  },
+  setup() {
+    const data = ref(['awesome', 'stuff', 'going', 'on']);
+    const name = ref('');
+    const filteredDataArray = computed(() => {
+      return data.value.filter(option => {
+        return (
+          option
+            .toString()
+            .toLowerCase()
+            .indexOf(name.value.toLowerCase()) >= 0
+        )
+      })
+    });
 
-        const tableData = ref([
-            {
-                id: 1,
-                first_name: "Jesse",
-                last_name: "Simmons",
-                date: "2016-10-15 13:43:27",
-                gender: "Male",
-            },
-            {
-                id: 2,
-                first_name: "John",
-                last_name: "Jacobs",
-                date: "2016-12-15 06:00:53",
-                gender: "Male",
-            },
-            {
-                id: 3,
-                first_name: "Tina",
-                last_name: "Gilbert",
-                date: "2016-04-26 06:26:28",
-                gender: "Female",
-            },
-            {
-                id: 4,
-                first_name: "Clarence",
-                last_name: "Flores",
-                date: "2016-04-10 10:28:46",
-                gender: "Male",
-            },
-            {
-                id: 5,
-                first_name: "Anne",
-                last_name: "Lee",
-                date: "2016-12-06 14:38:38",
-                gender: "Female",
-            },
+    const tableData = ref([
+          {
+            id: 1,
+            first_name: 'Jesse',
+            last_name: 'Simmons',
+            date: '2016-10-15 13:43:27',
+            gender: 'Male'
+          },
+          {
+            id: 2,
+            first_name: 'John',
+            last_name: 'Jacobs',
+            date: '2016-12-15 06:00:53',
+            gender: 'Male'
+          },
+          {
+            id: 3,
+            first_name: 'Tina',
+            last_name: 'Gilbert',
+            date: '2016-04-26 06:26:28',
+            gender: 'Female'
+          },
+          {
+            id: 4,
+            first_name: 'Clarence',
+            last_name: 'Flores',
+            date: '2016-04-10 10:28:46',
+            gender: 'Male'
+          },
+          {
+            id: 5,
+            first_name: 'Anne',
+            last_name: 'Lee',
+            date: '2016-12-06 14:38:38',
+            gender: 'Female'
+          }
         ]);
         const columns = ref([
-            {
-                field: "id",
-                label: "ID",
-                width: "40",
-                numeric: true,
-            },
-            {
-                field: "first_name",
-                label: "First Name",
-            },
-            {
-                field: "last_name",
-                label: "Last Name",
-            },
-            {
-                field: "date",
-                label: "Date",
-                position: "centered",
-            },
-            {
-                field: "gender",
-                label: "Gender",
-            },
+          {
+            field: 'id',
+            label: 'ID',
+            width: '40',
+            numeric: true
+          },
+          {
+            field: 'first_name',
+            label: 'First Name'
+          },
+          {
+            field: 'last_name',
+            label: 'Last Name'
+          },
+          {
+            field: 'date',
+            label: 'Date',
+            position: 'centered'
+          },
+          {
+            field: 'gender',
+            label: 'Gender'
+          }
         ]);
 
-        return { name, filteredDataArray, tableData, columns };
-    },
+    return { name, filteredDataArray, tableData, columns }
+  }
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
-    margin: 40px 0 0;
+  margin: 40px 0 0;
 }
 ul {
-    list-style-type: none;
-    padding: 0;
+  list-style-type: none;
+  padding: 0;
 }
 li {
-    display: inline-block;
-    margin: 0 10px;
+  display: inline-block;
+  margin: 0 10px;
 }
 a {
-    color: #42b983;
+  color: #42b983;
 }
 </style>
