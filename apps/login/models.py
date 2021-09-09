45836756fbcd3d8129fd66d0534d05b0d70167d0
tombufardeci <cgnewlistings@gmail.com>
2021-09-09 20:04:15 +1000

    Added the User model that stores the user's sign up data

--- apps/login/models.py
+++ apps/login/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    loginEmail = models.CharField(max_length=50, primary_key=True)
+    dateCreated = models.DateTimeField()
+    password = models.CharField(max_length=45)
+
+    #linkedToContactId = models.OneToOneField('Contact', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.loginEmail
 
