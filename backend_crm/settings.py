f2957a8ad3684785bb16209cc61d6c8bbf7569a4
Luis Fennessy <lfennessy@student.unimelb.edu.au>
2021-09-27 09:39:39 +1000

    changing settings for merge conflict

--- backend_crm/settings.py
+++ backend_crm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework'
     'rest_framework.authtoken'
+    
+    #our installed apps
+    'apps.calendar',
+    'apps.contact_book',
+    'apps.account'
 ]
 
 MIDDLEWARE = [
