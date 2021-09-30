4e0c8835eebccdbd90b971a8988a239100b0ceba
Shivanah Shetty <shivanahs@student.unimelb.edu.au>
2021-09-30 18:38:18 +0800

    migrated database tables and started initial back end tests

--- apps/account/tests.py
+++ apps/account/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from apps.account.models import AccountManager
+from apps.account.models import User
 
 # Create your tests here.
+
+
+class AccountManagerTests(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(email='shiv@gmail.com', password='foo')
+        self.assertEqual(user.email, 'shiv@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', password="foo")
+
+    def test_create_superuser(self):
+        admin_user = User.objects.create_superuser(email='supershiv@gmail.com', password='foo')
+        self.assertEqual(admin_user.email, 'supershiv@gmail.com')
+        self.assertTrue(admin_user.is_admin)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='super@user.com', password='foo', is_superuser=False)
+
+
