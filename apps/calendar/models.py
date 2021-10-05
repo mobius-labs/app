7a808d3f9026557dbd7a0d12c02ade7785ac8bcf
Luis Fennessy <lfennessy@student.unimelb.edu.au>
2021-10-05 20:02:34 +1100

    updated (inserted blanks, defaults) and commented models

--- apps/calendar/models.py
+++ apps/calendar/models.py
@@ -19,35 +19,40 @@ class EventResponse(Enum):
     DEC = "declined"
 
 
+# models an event to be included on the calendar
 class Event(models.Model):
 
     title = models.CharField(max_length=45)
-    start_time = models.DateTimeField(null=True)
-    end_time = models.DateTimeField(null=True)
-    is_all_day = models.BooleanField()
-    location = models.CharField(max_length=45, null=True)
-    notes = models.TextField(null=True)
-    url = models.CharField(max_length=300, null=True)
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    is_all_day = models.BooleanField(default=False)
+    location = models.CharField(max_length=45, null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    url = models.CharField(max_length=300, null=True, blank=True)
     tag = models.CharField(
         max_length=15,
-        choices=[(tag, tag.value) for tag in EventTag]
+        choices=[(tag, tag.value) for tag in EventTag],
+        null=True,
+        blank=True
     )
     response = models.CharField(
         max_length=15,
-        choices=[(tag, tag.value) for tag in EventResponse]
+        choices=[(tag, tag.value) for tag in EventResponse],
+        null=True,
+        blank=True
     )
-    is_public = models.BooleanField()
-    alert = models.DateTimeField(null=True)
-    login_of_host_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_public = models.BooleanField(default=True)
+    alert = models.DateTimeField(null=True, blank=True)
 
 
+# each event occurs any number of times.
 class EventOccurrence(models.Model):
 
     date = models.DateTimeField()
-    login_of_host_user = models.ForeignKey(User, on_delete=models.CASCADE)
     event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
 
 
+# to be properly modelled in the third sprint
 '''
 class EventIncludesContact(models.Model):
 
